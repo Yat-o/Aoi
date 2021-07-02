9937feb1e27845697c67a961ce969a40d50f5644
Yato <tylergoodmen312@gmail.com>
2021-07-02 02:21:53 -0400

    yesnt

--- bot/cogs/administration/jishaku.py
+++ bot/cogs/administration/jishaku.py
@@ -1,5 +1,16 @@
 from bot import aoi
 from jishaku.cog import Jishaku
 
-def setup(bot: aoi.AoiBot) -> None:
-    bot.add_cog(Jishaku(bot=bot))
+class MyJishaku(Jishaku):
+  @property
+  def description(self):
+    return "Jishaku"
+
+
+def setup(bot: aoi.AoiBot):
+    cog = MyJishaku(bot=bot)
+
+    for command in cog.walk_commands():
+      command.brief="Just know what it does"
+
+    bot.add_cog(cog)
